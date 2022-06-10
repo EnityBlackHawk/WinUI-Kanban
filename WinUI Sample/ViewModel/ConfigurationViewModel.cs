4ab984fd4f2cea05b931e4cc2b6eaa6590818f51
Luan Felipe <luanf2003@gmail.com>
2022-06-10 13:37:17 -0300

    Add a file picker

--- WinUI Sample/ViewModel/ConfigurationViewModel.cs
+++ WinUI Sample/ViewModel/ConfigurationViewModel.cs
@@ -12,6 +12,7 @@ namespace WinUI_Sample.ViewModel
         public List<string> Backgrounds { get; set; }
 
         public ButtonCommand SaveCommand { get; set; }
+        public ButtonCommand PathCommand { get; set; }
 
         private string _selectedBackground;
 
@@ -39,6 +40,7 @@ namespace WinUI_Sample.ViewModel
             SelectedBackground = _configurationManager.GetBackgroundType();
             Path = _configurationManager.GetBackgroundImagePath();
             SaveCommand = new ButtonCommand(save);
+            PathCommand = new ButtonCommand(GetPath);
         }
 
         private void save()
@@ -47,6 +49,22 @@ namespace WinUI_Sample.ViewModel
             _configurationManager.SetBackgroundImagePath(Path);
             _configurationManager.Save();
             App.GetService<View.ViewManager>().Navegate(App.GetService<View.TablesView>());
+        }
+
+        private async void GetPath()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".png");
+            picker.FileTypeFilter.Add(".jpeg");
+
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.GetService<MainWindow>());
+            WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            Path = file != null ? file.Path : Path;
         }
     }
 }
